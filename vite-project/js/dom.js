54bc2c35d52503ae15b7b72c622a8aa172256c2a
batyrkhana <batyrkhansa@gmail.com>
2023-12-27 01:21:49 -0500

    new

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,16 +1,31 @@
 const DOMSelectors = {
+<<<<<<< Updated upstream
     mainOutput: document.getElementById("main-output.card"),
+=======
+    mainOutput: document.getElementById("main-output-card"),
+
+>>>>>>> Stashed changes
     characterForm: document.getElementById("formCharacters"),
+    characterPageNum: document.getElementById("q-character-pageNum-input"),
     characterName: document.getElementById("q-character-name-input"),
     characterStatus: document.getElementById("q-character-status-input"),
     characterSpecies: document.getElementById("q-character-species-input"),
     characterGender: document.getElementById("q-character-gender-input"),
     characterLocation: document.getElementById("q-character-location-input"),
     characterSubmit: document.getElementById("q-character-submit"),
+
     locationsForm: document.getElementById("formLocations"),
+    locationsPageNum: document.getElementById("q-location-pageNum-input"),
     locationsName: document.getElementById("q-location-name-input"),
     locationsType: document.getElementById("q-location-type-input"),
     locationsDimension: document.getElementById("q-location-dimension-input"),
     locationsSubmit: document.getElementById("q-location-submit"),
+<<<<<<< Updated upstream
+=======
+    
+    deleteCard: document.getElementById("deleteCharacterBtn"),
+    modal: document.getElementById("characterModal"),
+    closeXBtn: document.querySelector('.close'),
+>>>>>>> Stashed changes
 }
 export {DOMSelectors};
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,7 +3,19 @@ import { DOMSelectors } from "./dom";
 
 const characterDataArray = [];
 
+<<<<<<< Updated upstream
 //Gets the URL from the URL Coverter function to get the data from the API.
+=======
+/* 
+async function getCharData - What it does:
+1. Grabs the URL from the URL Converter function to extract API data.
+2. Grabs the API response and saves it as a .json file.
+3. Inserts the extracted data into the empty characterDataArray.
+4. Creates a card for each element in the characterDataArray.
+5. After the cards have been created, clears the characterDataArray, setting it equal to 0.
+
+*/
+>>>>>>> Stashed changes
 async function getCharData(charURL) {
     try {
         //Looks at the status value and produces error messages based on it.
@@ -21,8 +33,17 @@ async function getCharData(charURL) {
         const characterData = await response.json();
         console.log(characterData.results);
         characterData.results.forEach(character => characterDataArray.push(character));
+<<<<<<< Updated upstream
         console.log(characterDataArray)
         //do foreach on each data and push it
+=======
+        console.log(characterDataArray) //Status on characterDataArray API data insert
+
+        characterDataArray.forEach((character) => createCharacterCard(character));
+        console.log("Character Card Created"); // Status on characterDataArray cards creation
+        characterDataArray.length = 0;
+        console.log(characterDataArray); // Status on cleared Data Array
+>>>>>>> Stashed changes
     } catch (error) {
         console.log("Oh no you got the following error! " + error);
     };
@@ -40,8 +61,9 @@ async function getLocnData(locnURL) {
                 throw new Error(response.status + ". Idk what to tell you, try searching for this error on the web.");
             };
         }; 
-        console.log(response);
+        console.log(response); //Status for the response
         const locationData = await response.json();
+<<<<<<< Updated upstream
         console.log(locationData.results);
         location
         //const locationDataArray = [];
@@ -52,10 +74,21 @@ async function getLocnData(locnURL) {
         });
         const dataNew = locationDataArray.push(locationData.results);
         console.log(dataNew);
+=======
+        console.log(locationData.results); //Status on the API data retrieval
+        locationData.results.forEach(location => locationDataArray.push(location));
+        console.log(locationDataArray) //Status on locationDataArray API data insert
+
+        locationDataArray.forEach((location)=>createLocationCard(location));
+        console.log("Location Card Created"); //Status on locationDataArray cards creation
+        locationDataArray.length = 0;
+        console.log(locationDataArray); // Status on cleared Data Array
+>>>>>>> Stashed changes
     } catch (error) {
         console.log("Oh no you got the following error! " + error);
     };
 };
+<<<<<<< Updated upstream
 function createCharacterCard(characterData){
     DOMSelectors.mainOutput.insertAdjacentHTML("beforeend",
     `<div class="created-card">
@@ -65,43 +98,153 @@ function createCharacterCard(characterData){
       <h3 class="card-artist">${DOMSelectors.albumArtist.value}</h3>
       <button class="delete">DELETE</button>
     </div>`);
+=======
+const createCharacterCard = function(characterDataArray){
+    if (characterDataArray.type === "") {characterDataArray.type = "unknown"};
+    DOMSelectors.mainOutput.insertAdjacentHTML("afterbegin",
+    `<div class="created-character-card">
+        <div class="created-character-card-data">
+            <img src=${characterDataArray.image} alt="${characterDataArray.name} image" class="created-character-img">
+            <h1 class="created-character-name">Character Name: ${characterDataArray.name}</h1>
+            <h2 class="created-character-gender">Gender: ${characterDataArray.gender}</h2>
+            <h2 class="created-character-location">Current Location: ${characterDataArray.location.name}</h2>
+            <h2 class="created-character-origin-name">Origin: ${characterDataArray.origin.name}</h2>
+            <h2 class="created-character-status">Status: ${characterDataArray.status}</h2>
+            <h2 class="created-character-species">Species: ${characterDataArray.species}</h2>
+            <h2 class="created-character-type">Type: ${characterDataArray.type}</h2>
+        </div>
+        <div class="created-character-btns">
+            <div class="created-character-deleteBtn">
+                <button class="deleteCharacterBtn">DELETE</button>
+            </div>
+            <div class="created-character-infoBtn">
+                <button class="infoCharacterBtn">More Information</button>
+            </div>
+        </div>
+    </div>`)
+>>>>>>> Stashed changes
 };
-function clearCharacterInput() {
+const createLocationCard = function(locationDataArray){
+  DOMSelectors.mainOutput.insertAdjacentHTML("afterbegin",
+  `<div class="created-location-card">
+  <div class="created-location-card-data">
+      <h1 class="created-location-name">Location Name: ${locationDataArray.name}</h1>
+      <h2 class="created-location-type">Location Type: ${locationDataArray.type}</h2>
+      <h2 class="created-location-dimension">Location Dimension: ${locationDataArray.dimension}</h2>
+  </div>
+  <div class="created-location-btns">
+      <div class="created-location-deleteBtn">
+          <button class="deleteLocationBtn">DELETE</button>
+      </div>
+      <div class="created-location-infoBtn">
+          <button class="infoLocationBtn">More Information</button>
+      </div>
+  </div>
+</div>`)
+};
+// Function to open the modal
+function openModal() {
+  DOMSelectors.modal.style.display = "block";
+  DOMSelectors.closeXBtn.addEventListener('click', closeModal);
+}
+
+// Function to close the modal
+function closeModal() {
+  DOMSelectors.modal.style.display = "none";
+  DOMSelectors.closeXBtn.removeEventListener('click', closeModal);
+}
+
+// Event listener for "More Information" button to open the modal
+DOMSelectors.mainOutput.addEventListener('click', function (event) {
+  if (event.target.classList.contains('infoCharacterBtn')) {
+      const characterId = event.target.closest('.created-character-card').dataset.characterId;
+      openModal(characterId);
+  }
+});
+
+function clearInputs() {
+    DOMSelectors.characterPageNum.value = "";
     DOMSelectors.characterForm.value = "";
     DOMSelectors.characterName.value = "";
     DOMSelectors.characterStatus.value = "";
     DOMSelectors.characterSpecies.value = "";
     DOMSelectors.characterGender.value = "";
     DOMSelectors.characterLocation.value = "";
-};
-function clearLocationsInput() {
     DOMSelectors.locationsName.value = "";
     DOMSelectors.locationsType.value = "";
     DOMSelectors.locationsDimension.value = "";
 };
+<<<<<<< Updated upstream
+=======
+DOMSelectors.mainOutput.addEventListener('click', function (event) {
+  if (event.target.classList.contains('deleteCharacterBtn')) {
+    if (!confirm("Do you really want to do this?")) {
+      e.preventDefault(); // ! => don't want to do this
+    } else {
+      const card = event.target.closest('.created-character-card');
+      if (card) {
+        card.remove(); // Remove the card from the DOM
+      }
+    }
+  } else if (event.target.classList.contains('deleteLocationBtn')) {
+    if (!confirm("Do you really want to do this?")) {
+      e.preventDefault(); // ! => don't want to do this
+    } else {
+      const cardlocc = event.target.closest('.created-location-card');
+      if (cardlocc) {
+        cardlocc.remove(); // Remove the card from the DOM
+      }
+    }
+  }
+});
+DOMSelectors.mainOutput.addEventListener('click', function (event) {
+  if (event.target.classList.contains('deleteLocationBtn')) {
+      if (!confirm("Do you really want to do this?")) {
+          e.preventDefault(); // ! => don't want to do this
+      } else {
+          const cardlocc = event.target.closest('.created-location-card');
+          if (cardlocc) {
+              cardlocc.remove(); // Remove the card from the DOM
+          }
+      }
+  }
+});
+>>>>>>> Stashed changes
 // Takes the values of the individual user input fields and adds them as URL parameters.
 // encodeURIComponent ensures that the replaced URL parameters would not interfere in the functionality of the URL
 DOMSelectors.characterForm.addEventListener('submit', function characterURLConverter(submitCharacter) {
     submitCharacter.preventDefault(); 
+    const charPageNum = encodeURIComponent(DOMSelectors.characterPageNum.value);
     const charName = encodeURIComponent(DOMSelectors.characterName.value);
     const charStatus = encodeURIComponent(DOMSelectors.characterStatus.value);
     const charSpecies = encodeURIComponent(DOMSelectors.characterSpecies.value);
     const charGender = encodeURIComponent(DOMSelectors.characterGender.value);
     const charLocation = encodeURIComponent(DOMSelectors.characterLocation.value);
-    const charURL = `https://rickandmortyapi.com/api/character/?page=&name=${charName}&status=${charStatus}&species=${charSpecies}&gender=${charGender}&location=${charLocation}`;
+    const charURL = `https://rickandmortyapi.com/api/character/?page=${charPageNum}&name=${charName}&status=${charStatus}&species=${charSpecies}&gender=${charGender}&location=${charLocation}`;
     getCharData(charURL);
+<<<<<<< Updated upstream
     clearCharacterInput();
     //createCard();
+=======
+    clearInputs();
+>>>>>>> Stashed changes
 });
 DOMSelectors.locationsForm.addEventListener('submit', function locationsURLConverter(submitLocation) {
     submitLocation.preventDefault();
+    const locnPageNum = encodeURIComponent(DOMSelectors.locationsPageNum.value);
     const locnName = encodeURIComponent(DOMSelectors.locationsName.value);
     const locnType = encodeURIComponent(DOMSelectors.locationsType.value);
     const locnDimension = encodeURIComponent(DOMSelectors.locationsDimension.value);
+<<<<<<< Updated upstream
     const locnURL = `https://rickandmortyapi.com/api/location/?page=&name=${locnName}&type=${locnType}&dimension${locnDimension}`;
     getLocnData(locnURL);
     clearLocationsInput();
     //createCard();
+=======
+    const locnURL = `https://rickandmortyapi.com/api/location/?page=${locnPageNum}&name=${locnName}&type=${locnType}&dimension${locnDimension}`;
+    getLocationData(locnURL);
+    clearInputs();
+>>>>>>> Stashed changes
 });
 
 //locationData.forEach((location)=>createCharacterCard(location));
